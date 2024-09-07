4d7a57ae8cb3f9def0c0d6cc5e202a3ef74dd262
Dulce Belman <jimedulbelman@gmail.com>
2024-09-06 19:24:16 -0600

    proyecto ts

---
+++ src/temas-ts/02-intefas.ts
@@ -0,0 +1,17 @@
+interface Alumno{
+    nombre:string;
+    edad:number;
+    email: string;
+    calificacion?:number;
+}
+
+
+
+
+const Alumno:Alumno=
+{
+    nombre:'mario',
+    edad:23,
+    email:'mario@gmail.com',
+    
+}
